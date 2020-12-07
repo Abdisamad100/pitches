689ded3edbaa898fd15e73b4aba5fc1373056fdf
abdisamad100 <abdisamadcade004@gmail.com>
2020-12-07 21:19:45 +0300

    Deployment to heroku

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:1234@localhost/pitches'
-
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
     #email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
