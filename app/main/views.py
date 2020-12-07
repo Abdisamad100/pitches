266535db8119f300568961279a7ee5388a54dd35
abdisamad100 <abdisamadcade004@gmail.com>
2020-12-07 11:41:10 +0300

    added pitches form

--- app/main/views.py
+++ app/main/views.py
@@ -83,6 +83,7 @@ def new_pitch():
         return redirect(url_for('main.index'))
 
     return render_template('pitch.html',form= form)
+    
 
 @main.route('/categories/<cate>')
 def category(cate):
